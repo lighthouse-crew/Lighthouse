03ed7b944014aa2b3808ee5df944cfaa51258b0c
Mitchell Gu <me@mitchgu.com>
2015-09-20 06:47:58 -0400

    move join and create btns to sidebar

--- Lighthouse/SidebarViewController.swift
+++ Lighthouse/SidebarViewController.swift
@@ -12,6 +12,10 @@ import Foundation
 
 class SidebarViewController : UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    @IBOutlet weak var addPartyButton: UIButton!
+    @IBOutlet weak var addHouseButton: UIButton!
+    @IBOutlet weak var joinButton: UIButton!
+    
     @IBOutlet weak var groupsTableView: UITableView!
     
     @IBOutlet weak var nameLabel: UILabel!
@@ -35,6 +39,122 @@ class SidebarViewController : UIViewController, UITableViewDelegate, UITableView
     
     func showNetworkErrorAlert() {
         showAlert("Network Error", message: "Please try again later. ")
+    }
+    
+    func processAddLightGroup(type: LightGroupType, name: String) {
+        var individual : Int
+        
+        switch (type) {
+        case .LightHouse:
+            individual = 1
+        case .LightParty:
+            individual = 0
+        }
+        
+        addHouseButton.enabled = false
+        addPartyButton.enabled = false
+        joinButton.enabled = false
+        
+        Alamofire.request(.POST, engineURL + "/light_groups/", parameters: [
+            "name": name,
+            "individual": individual,
+            "listed": 1,
+            "token": DataStore.sharedStore.token!
+            ]).responseJSON { (_, _, result) -> Void in
+                if (!result.isSuccess) {
+                    self.showNetworkErrorAlert()
+                } else {
+                    let value = result.value!
+                    
+                    if (value["success"] as! Int == 0) {
+                        self.showAlert("Error", message: (value["errors"] as! [String]).joinWithSeparator("\n"))
+                    } else {
+                        self.showAlert("Success", message: "Successfully created the light house/light party. ")
+                    }
+                }
+                
+                self.addHouseButton.enabled = true
+                self.addPartyButton.enabled = true
+                self.joinButton.enabled = true
+        }
+    }
+    
+    func processJoin(name: String) {
+        addHouseButton.enabled = false
+        addPartyButton.enabled = false
+        joinButton.enabled = false
+        
+        Alamofire.request(.POST, engineURL + "/light_groups/find_and_join", parameters: [
+            "token": DataStore.sharedStore.token!,
+            "name": name
+            ]).responseJSON { (_, _, result) -> Void in
+                if (!result.isSuccess) {
+                    self.showNetworkErrorAlert()
+                } else {
+                    let value = result.value!
+                    
+                    if (value["success"] as! Int == 1) {
+                        self.showAlert("Success", message: "You have successfully joined the group. ")
+                    } else {
+                        self.showAlert("Error", message: "Error joining the group. The group might not exist. ")
+                    }
+                }
+                
+                self.addHouseButton.enabled = true
+                self.addPartyButton.enabled = true
+                self.joinButton.enabled = true
+        }
+    }
+    
+    func addLightGroup(type : LightGroupType) {
+        var message : String
+        
+        switch (type) {
+        case .LightHouse:
+            message = "Create a new light house. "
+        case .LightParty:
+            message = "Create a new light party. "
+        }
+        
+        let alertController = UIAlertController(title: "Create", message: message, preferredStyle: .Alert)
+        
+        let addAction = UIAlertAction(title: "Create", style: .Default) { (_) -> Void in
+            let name = alertController.textFields![0]
+            self.processAddLightGroup(type, name: name.text!)
+        }
+        
+        alertController.addTextFieldWithConfigurationHandler { (textField) -> Void in
+            textField.placeholder = "Name"
+        }
+        
+        alertController.addAction(addAction)
+        
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func addHousePressed(sender: AnyObject) {
+        addLightGroup(LightGroupType.LightHouse)
+    }
+    
+    @IBAction func addPartyPressed(sender: AnyObject) {
+        addLightGroup(LightGroupType.LightParty)
+    }
+    
+    @IBAction func joinPartyPressed(sender: AnyObject) {
+        let alertController = UIAlertController(title: "Join", message: "Join an existing light house/light party. ", preferredStyle: .Alert)
+        
+        let joinAction = UIAlertAction(title: "Join", style: .Default) { (_) -> Void in
+            let name = alertController.textFields![0]
+            self.processJoin(name.text!)
+        }
+        
+        alertController.addTextFieldWithConfigurationHandler { (textField) -> Void in
+            textField.placeholder = "Name"
+        }
+        
+        alertController.addAction(joinAction)
+        
+        self.presentViewController(alertController, animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
